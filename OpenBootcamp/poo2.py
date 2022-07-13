4830aeaa6802e180235d7f1637fe1905a5226f2f
eddcf9 <315103243@pcpuma.acatlan.unam.mx>
2022-07-13 16:34:41 -0500

    Actualizando

--- OpenBootcamp/poo2.py
+++ OpenBootcamp/poo2.py
@@ -3,7 +3,14 @@ class Alumno():
     Nota = 10
 
     def mostrarAtributos(self):
-        return print("Hola",self.Nombre + ", tu calificacion es:",self.Nota)
+        return print("Hola",self.Nombre)
+
+    def isAprobatoria(self):
+        if self.Nota >= 6:
+            print("Felicidades!, Has aprobado con", self.Nota)
+        else:
+            print("Lo siento, no has logrado aprobar")
 
 A = Alumno()
 A.mostrarAtributos()
+A.isAprobatoria()
